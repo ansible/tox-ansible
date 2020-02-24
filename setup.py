708b85a56dc491102f46a8e04100e0bd976218f7
Gregory Hellings <greg.hellings@gmail.com>
2020-02-23 22:24:38 -0600

    Version 0.9.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='greg.hellings@gmail.com',
     url='https://github.com/greg-hellings/tox-ansible',
     license='GPLv3',
-    version='0.9',
+    version='0.9.1',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={
@@ -20,7 +20,7 @@ setup(
     install_requires=['tox>=2.4', 'pyyaml'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Framework :: tox',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
