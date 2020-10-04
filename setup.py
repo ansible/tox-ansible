a7b48c0308e5f9a1d329d1c9916e66d5d934189e
Sorin Sbarnea <ssbarnea@redhat.com>
2020-10-04 10:02:11 +0100

    Adopt setuptools packaging
    Moves package metadata to setup.cfg as setup.py was deprecated
    long time ago.
    
    This also means that now we get tag based versioning for free, one
    that works like pbr but without needing to use pbr.

--- setup.py
+++ setup.py
@@ -1,29 +1,7 @@
-import io
+import setuptools
 
-from setuptools import find_packages, setup
-
-setup(
-    name="tox-ansible",
-    description="Auto-generate environments for molecule role testing",
-    long_description_content_type="text/markdown",
-    long_description=io.open("README.md", encoding="utf-8").read(),
-    author="Greg Hellings",
-    author_email="greg.hellings@gmail.com",
-    url="https://github.com/ansible-community/tox-ansible",
-    license="GPLv3",
-    version="0.11.0",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    entry_points={"tox": ["ansible-collection = tox_ansible.hooks"]},
-    install_requires=["tox>=2.7", "pyyaml"],
-    python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Framework :: tox",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Testing",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup(
+        use_scm_version={"local_scheme": "no-local-version"},
+        setup_requires=["setuptools_scm[toml]>=3.5.0"],
+    )
