45499740812aad57faff3274897153bc459b13bc
Sorin Sbarnea <ssbarnea@redhat.com>
2024-11-05 17:58:18 +0000

    Add support for py313

--- src/tox_ansible/plugin.py
+++ src/tox_ansible/plugin.py
@@ -42,8 +42,9 @@ ALLOWED_EXTERNALS = [
 ENV_LIST = """
 {integration, sanity, unit}-py3.9-{2.15}
 {integration, sanity, unit}-py3.10-{2.15, 2.16, 2.17}
-{integration, sanity, unit}-py3.11-{2.15, 2.16, 2.17, milestone, devel}
-{integration, sanity, unit}-py3.12-{2.16, 2.17, milestone, devel}
+{integration, sanity, unit}-py3.11-{2.15, 2.16, 2.17, 2.18, milestone, devel}
+{integration, sanity, unit}-py3.12-{2.16, 2.17, 2.18, milestone, devel}
+{integration, sanity, unit}-py3.13-{2.18, milestone, devel}
 """
 TOX_WORK_DIR = Path()
 OUR_DEPS = [
