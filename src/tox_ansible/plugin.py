3b2f2d914c69e67555dd93fe7cad1f4385177513
Shatakshi Mishra <shatakshimishra01@gmail.com>
2025-05-26 16:14:28 +0530

    Remove support for ansible-core 2.16 due to EOL (#448)

--- src/tox_ansible/plugin.py
+++ src/tox_ansible/plugin.py
@@ -43,9 +43,9 @@ ALLOWED_EXTERNALS = [
     "dirname",
 ]
 ENV_LIST = """
-{integration, sanity, unit}-py3.10-{2.16, 2.17}
-{integration, sanity, unit}-py3.11-{2.16, 2.17, 2.18, milestone, devel}
-{integration, sanity, unit}-py3.12-{2.16, 2.17, 2.18, milestone, devel}
+{integration, sanity, unit}-py3.10-{2.17}
+{integration, sanity, unit}-py3.11-{2.17, 2.18, milestone, devel}
+{integration, sanity, unit}-py3.12-{2.17, 2.18, milestone, devel}
 {integration, sanity, unit}-py3.13-{2.18, milestone, devel}
 """
 TOX_WORK_DIR = Path()
